00a28f1111628359d2fe68baf8f5ec60b3ab97e0
abbatissa <sergey@ukhov.ru>
2019-11-21 02:43:46 +0700

    Доделана сетка, все адаптивно как конфетка. Сделаны макеты страниц, громе главной

---
+++ js/Sergeyvue.js
@@ -0,0 +1,148 @@
+new Vue ({
+    el: '#app1', 
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    },
+})
+new Vue ({
+    el: '#app2',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app3',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app4',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app5',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app6',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app7',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app8',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+new Vue ({
+    el: '#app9',
+    data: {
+        isActive: false,
+        color: 'blue'
+    },
+    computed: {
+        getCssClasses: function () {
+            return {
+                'red': this.isActive,
+                'darkgray': !this.isActive,
+                
+            }
+        }
+    }
+})
+
+
+
+

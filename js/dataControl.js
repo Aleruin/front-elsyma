081d719d479988b9d06543dbffa3537f44d7acf4
Basnor <basnor@mail.ru>
2019-11-26 19:14:19 +0700

    Поправлено оформление ЦАП входов

--- js/dataControl.js
+++ js/dataControl.js
@@ -1,51 +1,41 @@
-  new Vue({                                                   
-                                             
-  el: '#data',                                            
-                                                          
-  data: {                                                 
-    descretePerfoms: "DAC-val",
-    currentPerform: "DAC-val",                 
-    values: null                                          
-  },                          
-                                                
-  created: function () {                                  
+  new Vue({
+
+  el: '#data',
+
+  data: {
+    dacPerfoms: "DAC-val",
+    dacValues: null
+  },
+
+  created: function () {
     var self = this 
     self.fetchData() 
 
     setInterval(function () {
         self.fetchData() 
-    }, 5000)                          
-  },                                                 
-                                                        
-  watch: {                                           
-    currentPerfodescretePerfomsrm: 'fetchData'                        
-                  
-  },                                                 
-                                                      
-  filters: {                                              
-                                                    
-  }, 
+    }, 5000)
+  },
 
-  methods: {                                                
-    fetchData: function () {                                
-        var self = this         
-                                
-        this.getJSON()          
+  methods: {
+    fetchData: function () {
+        var self = this
+
+        this.getJSON()
           .then(function(res) {
             var after_party = JSON.parse(res);
-            self.values = after_party[self.currentPerform]           
-          })                                               
-                                                          
-    },   
-    getJSON: function() {                                 
+            self.dacValues = after_party[self.dacPerfoms]
+          })
+
+    },
+    getJSON: function() {
       return new Promise((resolve, reject) => {
           const xhr = new XMLHttpRequest();
-          xhr.open("GET", "/tmlbdata");                  
-          xhr.onload = () => resolve(xhr.response);     
-          xhr.onerror = () => reject(xhr.statusText);   
-          xhr.send();                                   
-      });                                               
+          xhr.open("GET", "/tmlbdata");
+          xhr.onload = () => resolve(xhr.response);
+          xhr.onerror = () => reject(xhr.statusText);
+          xhr.send();
+      });
     }
-  }  
+  }
 
 })

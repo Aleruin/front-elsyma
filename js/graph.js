2cd6953e129632d249b97192e90e90e9a29738e6
eugeny <Tosvering@gmail.com>
2019-11-28 13:42:21 +0700

    Forget change smthng

--- js/graph.js
+++ js/graph.js
@@ -95,7 +95,7 @@ new Vue({
         {
             return new Promise((resolve, reject) => {
                 const xhr = new XMLHttpRequest();
-                xhr.open("GET", "http://192.168.56.101/data");
+                xhr.open("GET", "data");
                 xhr.onload = () => resolve(xhr.response);
                 xhr.onerror = () => reject(xhr.statusText);          
                 xhr.send();                                            

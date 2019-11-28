4de1efd6b6ec8bbd20ed283b8a8c6ccabd112782
eugeny <Tosvering@gmail.com>
2019-11-28 10:54:56 +0700

    Bug fix

--- js/graph.js
+++ js/graph.js
@@ -1,25 +1,27 @@
-new Vue({                                                    
+new Vue({
     el: '#container',                                            
     data: {                                                  
         data_container: [],
         history_container: [],
-        config: {                                             
-            type: 'line',                                          
-            data: {                                           
-                labels: [0],                                     
-                    datasets: [{                              
-                        label: 'Dataset1',                           
-                        fill: false,                                 
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',       
-                        borderColor: 'rgba(0, 0, 0, 0.5)',           
-                        borderWidth: 1,                          
-                        data: [0]                                
-                    },                                           
-                    {                                            
-                        label: 'Dataset2',                         
-                        fill: false,                                 
-                        backgroundColor: 'rgba(255, 69, 0, 0.5)',    
-                        borderColor: 'rgba(255, 69, 0, 0.5)',        
+        type_container: [],
+        diag_container: [],
+        config: {
+            type: 'line',
+            data: {
+                labels: [0],
+                    datasets: [{
+                        label: 'Dataset1',
+                        fill: false,
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        borderColor: 'rgba(0, 0, 0, 0.5)',
+                        borderWidth: 1,
+                        data: [0]
+                    },
+                    {
+                        label: 'Dataset2',
+                        fill: false,
+                        backgroundColor: 'rgba(255, 69, 0, 0.5)',
+                        borderColor: 'rgba(255, 69, 0, 0.5)',
                         borderWidth: 1,                              
                         data: [0]                                    
                     },                                               
@@ -58,67 +60,17 @@ new Vue({
         let line = new Chart(ctx, this.config)                     
         let self = this                                            
                                                                    
-        setInterval(function()story_container: [],                 
-        config: {                                                  
-            type: 'line',                                            
-            data: {                                                  
-                labels: [0],                                         
-                    datasets: [{                                     
-                        label: 'АЦП-1',                         
-                        fill: false,                               
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',     
-                        borderColor: 'rgba(0, 0, 0, 0.5)',         
-                        borderWidth: 1,                            
-                        data: [0]                                    
-                    },                                               
-                    {                                                
-                        label: 'АЦП-2,                           
-                        fill: false,                                 
-                        backgroundColor: 'rgba(255, 69, 0, 0.5)',    
-                        borderColor: 'rgba(255, 69, 0, 0.5)',        
-                        borderWidth: 1,                          
-                        data: [0]                                
-                    },                                               
-                    {                                                
-                        label: 'АЦП-3',                           
-                        fill: false,                                 
-                        backgroundColor: 'rgba(75, 0, 130, 0.5)',  
-                        borderColor: 'rgba(75, 0, 130, 0.5)',      
-                        borderWidth: 1,                              
-                        data: [0]                                    
-                    },                                               
-                    {                                                
-                        label: 'АЦП-4',                         
-                        fill: false,                               
-                        backgroundColor: 'rgba(0, 0, 255, 0.5)',     
-                        borderColor: 'rgba(0, 0, 255, 0.5)',         
-                        borderWidth: 1,                              
-                        data: [0]                                    
-                    }]                                               
-            },                                                     
-            options: {                                               
-                scales: {                                              
-                    yAxes: [{                                        
-                        ticks: {                                     
-                            suggestedMin: 50,                      
-                            suggestedMax: 100                      
-                        }
-			}]                                           
-                }                                                
- {                                                               
+        setInterval(function(){                                                               
             self.setTime(10);                                      
             self.loadData(line);                                   
-            self.getData();                                        
+            self.getData();
         }, 1000)                                                   
     },                                                             
     methods: {                                                     
-        randomScalingFactor: function() {                            
-            return Math.round(Math.random() * 100);                  
-        },                                                           
         loadData: function(chart) {                                  
             this.config.type = 'line'
             let history_str = String(this.data_container[0])
-                                                                     
+
             let history_arr = history_str.split(',')
             history_arr.pop()
             
@@ -134,19 +86,19 @@ new Vue({
             this.time += 1                                           
                                                                      
             chart.update()                                           
-        },                                                           
-        setTime: function(maxTime) {                                 
-            if( this.time >= maxTime-1 ) {                           
-                this.config.data.labels.shift()                      
-                this.config.data.datasets[0].data.shift()            
-            }                                                        
-        },                                                           
-        getJSON_data: function()                                     
-        {                                                            
-            return new Promise((resolve, reject) => {                
-                const xhr = new XMLHttpRequest();                    
-                xhr.open("GET", "data");                             
-                xhr.onload = () => resolve(xhr.response);            
+        },
+        setTime: function(maxTime) {
+            if( this.time >= maxTime-1 ) {
+                this.config.data.labels.shift()
+                this.config.data.datasets[0].data.shift()
+            }
+        },
+        getJSON_data: function()
+        {
+            return new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", "data");
+                xhr.onload = () => resolve(xhr.response);
                 xhr.onerror = () => reject(xhr.statusText);          
                 xhr.send();                                            
             });                                                      
@@ -154,14 +106,19 @@ new Vue({
         getData: function()                                            
         {                                                            
             var self = this
-	    self.data_container = []                              
-            this.getJSON_data()                                      
-            .then(function(res)                                      
-            {                                                        
-                var aft_parse = JSON.parse(res)                      
+            self.data_container = []
+            this.getJSON_data()
+            .then(function(res)
+            {
+                var aft_parse = JSON.parse(res)
                 self.data_container.push(aft_parse['ADC-val'] + ',')
-                //console.log(self.data_container)                  
-            })                                                         
-        }                                                              
-    }                                                                  
+                self.type_container = (aft_parse['ADC-type'])
+                console.log(self.type_container)
+                console.log(typeof(self.type_container))
+                self.diag_container = (aft_parse['ADC-diag'])
+                console.log(self.diag_container)
+                console.log(typeof(self.diag_container))
+            })
+        }
+    }
 });
